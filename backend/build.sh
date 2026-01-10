0f8ca7b570618ef78fb8233c58465a196182b983
gyanaranjan sahoo <sahoogyanaranjan353@gmail.com>
2026-01-10 19:40:22 +0530

    Remove debug commands from build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -2,11 +2,6 @@
 # exit on error
 set -o errexit
 
-echo "Current working directory: $(pwd)"
-ls -la
-cat requirements.txt
-
-pip install --upgrade pip
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
